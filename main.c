48a9ba6fd52bb07c66e3a5be91a1ddbf4c1b2657
Thor Grimner <senseitg@gmail.com>
2014-11-29 11:27:46 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "config.h"
+#include "intrinsics.h"
+#include "fpmath.h"
+#include "fft.h"
+
+int main() {
+  // No examples at this time
+}
